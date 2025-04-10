6d2e9c8d976babe5c73a7616c4767ad2eef379f2
ui.alpharive <ui.alpharive@gmail.com>
2025-04-10 15:30:27 +0530

    aboutus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 // common header
 fetch("header.html")
-  .then(response => response.text())
-  .then(data => {
+  .then((response) => response.text())
+  .then((data) => {
     document.getElementById("header").innerHTML = data;
+  });
+// common header
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
   });
 
 // header sticky
-  $(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) { // Change 100 to the height you want to trigger the effect
-            $('.header-section ').addClass('is-sticky');
-        } else {
-            $('.header-section ').removeClass('is-sticky');
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      // Change 100 to the height you want to trigger the effect
+      $(".header-section ").addClass("is-sticky");
+    } else {
+      $(".header-section ").removeClass("is-sticky");
+    }
+  });
 });
-
